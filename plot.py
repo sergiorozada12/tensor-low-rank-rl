150a7955f22531787b0530b386f9391b061c6452
E054031 <sergio.rozada@bbva.com>
2021-03-06 19:56:30 +0100

    feature:  plot py script added

---
+++ plot.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import Saver
+
+saver = Saver()
+Q = saver.load_from_pickle("models/pendulum_Q_svd.pck")
+
+_, sigma, _ = np.linalg.svd(Q.reshape(-1, Q.shape[-1]))
+
+plt.figure()
+plt.bar(np.arange(len(sigma)), sigma)
+plt.show()

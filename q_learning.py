9d425717a8191ffc2127087af11d393c9d18f78c
E054031 <sergio.rozada@bbva.com>
2021-03-06 18:07:16 +0100

    feature: Q learning experiment to obtain svd

---
+++ q_learning.py
@@ -0,0 +1,29 @@
+import gym
+import json
+from utils import Discretizer, Saver
+from models import QLearning
+
+parameters_file = "experiments/pendulum_svd.json"
+with open(parameters_file) as j:
+    parameters = json.loads(j.read())
+
+env = gym.make('Pendulum-v0')
+saver = Saver()
+
+discretizer = Discretizer(min_points_states=parameters["min_states"],
+                          max_points_states=parameters["max_states"],
+                          spacing_states=parameters["spacing_states"],
+                          min_points_actions=parameters["min_actions"],
+                          max_points_actions=parameters["max_actions"],
+                          spacing_actions=parameters["spacing_actions"])
+
+q_learner = QLearning(env=env,
+                      discretizer=discretizer,
+                      episodes=parameters["episodes"],
+                      max_steps=parameters["max_steps"],
+                      epsilon=parameters["epsilon"],
+                      alpha=parameters["alpha"],
+                      gamma=parameters["gamma"])
+
+q_learner.train()
+saver.save_to_pickle("models/pendulum_svd.pck", q_learner)

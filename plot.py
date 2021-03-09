4ea40e4c2ad9bd21dedfcfbaddbb708d3e85a95d
E054031 <sergio.rozada@bbva.com>
2021-03-10 00:57:37 +0100

    refactor: plot rearranged

--- plot.py
+++ plot.py
@@ -4,19 +4,13 @@ from utils import Saver
 
 saver = Saver()
 
-Q = saver.load_from_pickle("models/pendulum_Q_svd.pck")
+Q_svd_pendulum = saver.load_from_pickle("models/pendulum_Q_svd.pck")
+Q_svd_cartpole = saver.load_from_pickle("models/cartpole_Q_svd.pck")
 
-_, sigma, _ = np.linalg.svd(Q.reshape(-1, Q.shape[-1]))
+_, sigma_pendulum, _ = np.linalg.svd(Q_svd_pendulum.reshape(-1, Q_svd_pendulum.shape[-1]))
+_, sigma_cartpole, _ = np.linalg.svd(Q_svd_cartpole.reshape(-1, Q_svd_cartpole.shape[-1]))
 
-plt.figure()
-plt.bar(np.arange(len(sigma)), sigma)
-plt.show()
-
-#Q = saver.load_from_pickle("models/pendulum_Q_svd.pck")
-Q = saver.load_from_pickle("models/cartpole_Q_svd.pck")
-
-_, sigma, _ = np.linalg.svd(Q.reshape(-1, Q.shape[-1]))
-
-plt.figure()
-plt.bar(np.arange(len(sigma)), sigma)
+fig, axes = plt.subplots(nrows=1, ncols=2)
+axes[0].bar(np.arange(len(sigma_pendulum)), sigma_pendulum)
+axes[1].bar(np.arange(len(sigma_cartpole)), sigma_cartpole)
 plt.show()

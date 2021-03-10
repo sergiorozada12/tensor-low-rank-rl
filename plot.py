5ca510eaa390ea6bc85333f8b1c1f2a1660a5634
E054031 <sergio.rozada@bbva.com>
2021-03-10 01:24:43 +0100

    feature: mountain car plot added

--- plot.py
+++ plot.py
@@ -6,11 +6,14 @@ saver = Saver()
 
 Q_svd_pendulum = saver.load_from_pickle("models/pendulum_Q_svd.pck")
 Q_svd_cartpole = saver.load_from_pickle("models/cartpole_Q_svd.pck")
+Q_svd_mountaincar = saver.load_from_pickle("models/mountaincar_Q_svd.pck")
 
 _, sigma_pendulum, _ = np.linalg.svd(Q_svd_pendulum.reshape(-1, Q_svd_pendulum.shape[-1]))
 _, sigma_cartpole, _ = np.linalg.svd(Q_svd_cartpole.reshape(-1, Q_svd_cartpole.shape[-1]))
+_, sigma_mountaincar, _ = np.linalg.svd(Q_svd_mountaincar.reshape(-1, Q_svd_mountaincar.shape[-1]))
 
 fig, axes = plt.subplots(nrows=1, ncols=2)
 axes[0].bar(np.arange(len(sigma_pendulum)), sigma_pendulum)
 axes[1].bar(np.arange(len(sigma_cartpole)), sigma_cartpole)
+axes[2].bar(np.arange(len(sigma_mountaincar)), sigma_mountaincar)
 plt.show()

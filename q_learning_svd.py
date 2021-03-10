7c954fd56d23432eafa2941c1993c4e1e6ce25d9
E054031 <sergio.rozada@bbva.com>
2021-03-10 01:11:10 +0100

    feature: mountaincar env added

--- q_learning_svd.py
+++ q_learning_svd.py
@@ -2,15 +2,17 @@ import gym
 import json
 from utils import Discretizer, Saver
 from models import QLearning
-from environments import ContinuousCartPoleEnv
+from environments import ContinuousCartPoleEnv, Continuous_MountainCarEnv
 
 #parameters_file = "experiments/pendulum_svd.json"
-parameters_file = "experiments/cartpole_svd.json"
+#parameters_file = "experiments/cartpole_svd.json"
+parameters_file = "experiments/mountaincar_svd.json"
 with open(parameters_file) as j:
     parameters = json.loads(j.read())
 
 #env = gym.make('Pendulum-v0')
-env = ContinuousCartPoleEnv()
+#env = ContinuousCartPoleEnv()
+env = Continuous_MountainCarEnv()
 saver = Saver()
 
 discretizer = Discretizer(min_points_states=parameters["min_states"],
@@ -26,7 +28,10 @@ q_learner = QLearning(env=env,
                       max_steps=parameters["max_steps"],
                       epsilon=parameters["epsilon"],
                       alpha=parameters["alpha"],
-                      gamma=parameters["gamma"])
+                      gamma=parameters["gamma"],
+                      decay=decay["decay"])
 
 q_learner.train()
-saver.save_to_pickle("models/cartpole_Q_svd.pck", q_learner.Q)
+#saver.save_to_pickle("models/pendulum_Q_svd.pck", q_learner.Q)
+#saver.save_to_pickle("models/cartpole_Q_svd.pck", q_learner.Q)
+saver.save_to_pickle("models/mountaincar_Q_svd.pck", q_learner.Q)

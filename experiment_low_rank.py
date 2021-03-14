35567b655b59f78c2d494547422d8e0b02d74fe6
E054031 <sergio.rozada@bbva.com>
2021-03-14 12:44:10 +0100

    fix: name minors fixed

--- experiment_low_rank.py
+++ experiment_low_rank.py
@@ -13,4 +13,5 @@ with open(parameters_file_pend_lr) as j: parameters_pend_lr = json.loads(j.read(
 
 env_pend = gym.make('Pendulum-v0')
 
-run_q_learning_experiments(env_pend, parameters_file_pend_ql_state, "models/pendulum_ql_state_bucket_{}_exp_{}.pck", False, True)
+run_q_learning_experiments(env_pend, parameters_file_pend_ql_action, "models/pendulum_ql_action_bucket_{}_exp_{}.pck", False, True)
+run_q_learning_experiments(env_pend, parameters_file_pend_ql_state, "models/pendulum_ql_state_bucket_{}_exp_{}.pck", False, False)

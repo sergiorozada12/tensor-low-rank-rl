77cdf4d69cff42435a638620c2ff6f693d911b45
E054031 <sergio.rozada@bbva.com>
2021-03-17 19:48:57 +0100

    fix: minor in plots

--- plot.py
+++ plot.py
@@ -7,11 +7,9 @@ Plotter.plot_svd(["models/pendulum_Q_svd.pck",
                   "models/mountaincar_Q_svd.pck"])
 
 base_paths = ["models/pendulum_ql_action_bucket_{}_exp_{}.pck",
-              "models/pendulum_ql_state_bucket_{}_exp_{}.pck",
               "models/pendulum_lr_k_bucket_{}_exp_{}.pck"]
 
 experiment_paths = ["experiments/pendulum_low_rank_q_learner_action.json",
-                    "experiments/pendulum_low_rank_q_learner_state.json",
                     "experiments/pendulum_low_rank_lr_learner.json"]
 
 Plotter.plot_rewards(base_paths, experiment_paths)

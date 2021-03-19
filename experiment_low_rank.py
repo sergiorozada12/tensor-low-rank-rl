99c51e26af1570609224c17a259de7fbf3103d93
E054031 <sergio.rozada@bbva.com>
2021-03-19 17:33:37 +0100

    fix: fixed minors in parameters and models

--- experiment_low_rank.py
+++ experiment_low_rank.py
@@ -24,5 +24,5 @@ env_cart = ContinuousCartPoleEnv()
 #Experiment.run_q_learning_experiments(env_pend, parameters_pend_ql_state, "models/pendulum_ql_state_bucket_{}_exp_{}.pck", False, False)
 #Experiment.run_lr_learning_experiments(env_pend, parameters_pend_lr, "models/pendulum_lr_k_bucket_{}_exp_{}.pck", True)
 
-Experiment.run_q_learning_experiments(env_cart, parameters_cart_ql_action, "models/cartpole_ql_action_bucket_{}_exp_{}.pck", True, True)
-Experiment.run_lr_learning_experiments(env_cart, parameters_cart_lr, "models/cartpole_lr_k_bucket_{}_exp_{}.pck", True, True)
+#Experiment.run_q_learning_experiments(env_cart, parameters_cart_ql_action, "models/cartpole_ql_action_bucket_{}_exp_{}.pck", True, True)
+Experiment.run_lr_learning_experiments(env_cart, parameters_cart_lr, "models/cartpole_lr_k_bucket_{}_exp_{}.pck", True)
--- plot.py
+++ plot.py
@@ -6,10 +6,22 @@ Plotter.plot_svd(["models/pendulum_Q_svd.pck",
                   "models/cartpole_Q_svd.pck",
                   "models/mountaincar_Q_svd.pck"])
 
+##############################################################################
+
 base_paths = ["models/pendulum_ql_action_bucket_{}_exp_{}.pck",
               "models/pendulum_lr_k_bucket_{}_exp_{}.pck"]
 
 experiment_paths = ["experiments/pendulum_low_rank_q_learner_action.json",
                     "experiments/pendulum_low_rank_lr_learner.json"]
+
+Plotter.plot_rewards(base_paths, experiment_paths)
+
+#############################################################################
+
+base_paths = ["models/cartpole_ql_action_bucket_{}_exp_{}.pck",
+              "models/cartpole_lr_k_bucket_{}_exp_{}.pck"]
+
+experiment_paths = ["experiments/cartpole_low_rank_q_learner_action.json",
+                    "experiments/cartpole_low_rank_lr_learner.json"]
 
 Plotter.plot_rewards(base_paths, experiment_paths)

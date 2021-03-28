f7de942b492f13e980a784db0d5e61763a8e7400
E054031 <sergio.rozada@bbva.com>
2021-03-28 22:51:18 +0200

    fix: new paths in plot

--- plot.py
+++ plot.py
@@ -3,19 +3,28 @@ from utils import Saver, Plotter
 saver = Saver()
 
 # PLOT 1
-Plotter.plot_svd(["models/pendulum_ql_action_bucket_5_exp_0.pck",
-                  "models/cartpole_ql_action_bucket_4_exp_0.pck",
-                  "models/mountaincar_ql_action_bucket_4_exp_0.pck"])
+Plotter.plot_svd(["models/low_rank_reward/pendulum_ql_action_bucket_5_exp_0.pck",
+                  "models/low_rank_reward/cartpole_ql_action_bucket_4_exp_0.pck",
+                  "models/low_rank_reward/mountaincar_ql_action_bucket_4_exp_0.pck"])
 
 # PLOT 2
-base_paths_pend = ["models/pendulum_ql_action_bucket_{}_exp_{}.pck", "models/pendulum_lr_k_bucket_{}_exp_{}.pck"]
-experiment_paths_pend = ["experiments/pendulum_low_rank_q_learner_action.json", "experiments/pendulum_low_rank_lr_learner.json"]
+base_paths_pend = ["models/low_rank_reward/pendulum_ql_action_bucket_{}_exp_{}.pck",
+                   "models/low_rank_reward/pendulum_lr_k_bucket_{}_exp_{}.pck"]
 
-base_paths_cart = ["models/cartpole_ql_action_bucket_{}_exp_{}.pck", "models/cartpole_lr_k_bucket_{}_exp_{}.pck"]
-experiment_paths_cart = ["experiments/cartpole_low_rank_q_learner_action.json", "experiments/cartpole_low_rank_lr_learner.json"]
+experiment_paths_pend = ["experiments/low_rank_reward/pendulum_low_rank_q_learner_action.json",
+                         "experiments/low_rank_reward/pendulum_low_rank_lr_learner.json"]
 
-base_paths_mount = ["models/mountaincar_ql_action_bucket_{}_exp_{}.pck", "models/mountaincar_lr_k_bucket_{}_exp_{}.pck"]
-experiment_paths_mount = ["experiments/mountaincar_low_rank_q_learner_action.json", "experiments/mountaincar_low_rank_lr_learner.json"]
+base_paths_cart = ["models/low_rank_reward/cartpole_ql_action_bucket_{}_exp_{}.pck",
+                   "models/cartpole_lr_k_bucket_{}_exp_{}.pck"]
+
+experiment_paths_cart = ["experiments/low_rank_reward/cartpole_low_rank_q_learner_action.json",
+                         "experiments/cartpole_low_rank_lr_learner.json"]
+
+base_paths_mount = ["models/low_rank_reward/mountaincar_ql_action_bucket_{}_exp_{}.pck",
+                    "models/low_rank_reward/mountaincar_lr_k_bucket_{}_exp_{}.pck"]
+
+experiment_paths_mount = ["experiments/low_rank_reward/mountaincar_low_rank_q_learner_action.json",
+                          "experiments/low_rank_reward/mountaincar_low_rank_lr_learner.json"]
 
 Plotter.plot_rewards([base_paths_pend, base_paths_cart, base_paths_mount],
                      [experiment_paths_pend, experiment_paths_cart, experiment_paths_mount])

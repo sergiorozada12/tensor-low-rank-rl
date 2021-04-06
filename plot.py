cdb75a98de682db4c4056b89377b5034598f2d17
Sergio <sergiorozada12@gmail.com>
2021-04-06 23:33:57 +0200

    feature: base version to server

--- plot.py
+++ plot.py
@@ -3,8 +3,9 @@ from utils import Saver, Plotter
 saver = Saver()
 
 # PLOT 1
+"""
 Plotter.plot_svd(["models/low_rank_reward/pendulum_ql_action_bucket_5_exp_0.pck",
-                  "models/low_rank_reward/mountaincar_ql_action_bucket_4_exp_0.pck"])
+                  "models/low_rank_reward/mountaincar_ql_action_bucket_4_exp_0.pck"])"""
 
 # PLOT 2
 base_paths_pend = ["models/low_rank_reward/pendulum_ql_action_bucket_{}_exp_{}.pck",
@@ -25,14 +26,15 @@ base_paths_mount = ["models/low_rank_reward/mountaincar_ql_action_bucket_{}_exp_
 experiment_paths_mount = ["experiments/mountaincar_low_rank_q_learner_action.json",
                           "experiments/mountaincar_low_rank_lr_learner.json"]
 
-Plotter.plot_rewards([base_paths_pend, base_paths_mount],
-                     [experiment_paths_pend, experiment_paths_mount])
+Plotter.plot_rewards([base_paths_pend, base_paths_pend],
+                     [experiment_paths_pend, experiment_paths_pend])
 
 # Plot 3
+"""
 base_paths_pend = ["models/low_rank_convergence/pendulum_ql_action_bucket_{}_exp_{}.pck",
                    "models/low_rank_convergence/pendulum_lr_k_bucket_{}_exp_{}.pck"]
 
 Plotter.plot_convergence([base_paths_pend, base_paths_pend],
                          [experiment_paths_pend, experiment_paths_pend],
                          step_diff=10000,
-                         th=0.001)
+                         th=0.001)"""

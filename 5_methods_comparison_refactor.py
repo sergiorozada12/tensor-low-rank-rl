67b06da26100a1d9b3bf36c7cc3ad1776abc8aca
sergiorozada12 <sergiorozada12@gmail.com>
2022-03-27 16:21:30 +0200

    fix: minor in param arch name

--- 5_methods_comparison_refactor.py
+++ 5_methods_comparison_refactor.py
@@ -15,6 +15,9 @@ env_goddard = CustomGoddardEnv()
 if __name__ == "__main__":
     # Pendulum
     experiments = [f for f in os.listdir('parameters') if 'pendulum' in f]
+    experiments_done = [f for f in os.listdir('results') if 'pendulum' in f]
     for name in experiments:
+        if name in experiments_done:
+            continue
         experiment = Experiment(name, env_pendulum, 1)
         experiment.run_experiments(window=70)

85310c3c2df6696f958b37d23d55e6248a1729e6
sergiorozada12 <sergiorozada12@gmail.com>
2022-12-29 16:52:03 +0100

    feature: added highway env

--- src/environments/highway.py
+++ src/environments/highway.py
@@ -3,9 +3,17 @@ import highway_env
 
 
 class CustomHighwayEnv:
+    """
+    ACTIONS_ALL = {
+        0: 'LANE_LEFT',
+        1: 'IDLE',
+        2: 'LANE_RIGHT',
+        3: 'FASTER',
+        4: 'SLOWER'
+    }
+    """
     def __new__(cls):
         env = gym.make("highway-v0")
-        env.env.config['lanes_count'] = 3
 
         env.configure({
             "observation": {
@@ -19,8 +27,24 @@ class CustomHighwayEnv:
                     "vy": [-20, 20]
                 },
                 "absolute": False,
-                "order": "sorted"
-            }
+                "normalize": False,
+                "order": "sorted",
+            },
+            "lanes_count": 3,
+            "vehicles_count": 10,
+            "simulation_frequency": 5,
+            "duration": 50,
+            "collision_reward": -1.0,    # -1 The reward received when colliding with a vehicle.
+            "right_lane_reward": 0,  # 0.1 The reward received when driving on the right-most lanes, zero for other lanes.
+            "high_speed_reward": 0.4,  # 0.4 The reward driving at full speed, zero for lower speeds according to config["reward_speed_range"].
+            "lane_change_reward": 0,   # The reward received at each lane change action.
+            "reward_speed_range": [10, 30],
+            "normalize_reward": False, # Does not work
+            "disable_collision_checks":  True,
+            "initial_lane_id": 0,
+            "vehicle_density": 1,
+            "other_vehicles_type": 'highway_env.vehicle.behavior.IDMVehicle',
+            "ego_spacing": 2,
         })
 
         return env

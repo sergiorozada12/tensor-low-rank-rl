22eb53cc7d9fb07e00465b23cde34e7617a1bc69
sergiorozada12 <sergiorozada12@gmail.com>
2023-01-12 00:16:25 +0100

    fix: refactored first fig

--- 1_exp_svd_policy_iteration.py
+++ 1_exp_svd_policy_iteration.py
@@ -3,11 +3,13 @@ import gym_classics
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
+from src.utils.utils import OOMFormatter
 from src.algorithms.policy_iteration import PolicyIteration, PolicyIterationClassic
 
 
-env = gym.make('FrozenLake8x8-v0')
+env = gym.make('FrozenLake8x8-v1')
 q = PolicyIteration(env=env, max_iter=10000, k=1000, gamma=0.95).run()
 q -= np.mean(q)
 _, sigma_frozenlake, _ = np.linalg.svd(q)
@@ -36,26 +38,32 @@ with plt.style.context(['science'], ['ieee']):
     axes[0].bar(np.arange(1, len(sigma_frozenlake) + 1), sigma_frozenlake)
     axes[0].set_xlabel("(a) SV index")
     axes[0].set_ylabel("$\sigma$")
-    axes[0].ticklabel_format(style = 'sci', axis='y', scilimits=(0,0))
-    axes[0].xaxis.get_major_locator().set_params(integer=True)
+    axes[0].set_ylim(0, 2.1)
+    axes[0].set_yticks([0, 1, 2])
+    axes[0].yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
 
     axes[1].bar(np.arange(1, len(sigma_racetrack) + 1), sigma_racetrack)
     axes[1].set_xlabel("(b) SV index")
     axes[1].set_ylabel("$\sigma$")
+    axes[1].set_yticks([0, 200, 400])
+    axes[1].set_xticks([2, 4, 6, 8])
+    axes[1].yaxis.set_major_formatter(OOMFormatter(3, "%1.1f"))
     axes[1].ticklabel_format(style = 'sci', axis='y', scilimits=(0,0))
-    axes[1].xaxis.get_major_locator().set_params(integer=True)
 
     axes[2].bar(np.arange(1, len(sigma_rental) + 1), sigma_rental)
     axes[2].set_xlabel("(c) SV index")
     axes[2].set_ylabel("$\sigma$")
+    axes[2].set_yticks([0, 2500, 5000])
+    axes[2].yaxis.set_major_formatter(OOMFormatter(4, "%1.1f"))
     axes[2].ticklabel_format(style = 'sci', axis='y', scilimits=(0,0))
-    axes[2].xaxis.get_major_locator().set_params(integer=True)
 
     axes[3].bar(np.arange(1, len(sigma_taxi) + 1), sigma_taxi)
     axes[3].set_xlabel("(d) SV index")
     axes[3].set_ylabel("$\sigma$")
+    axes[3].set_yticks([0, 500, 1000])
+    axes[3].yaxis.set_major_formatter(OOMFormatter(3, "%1.1f"))
     axes[3].ticklabel_format(style = 'sci', axis='y', scilimits=(0,0))
-    axes[3].xaxis.get_major_locator().set_params(integer=True)
+    axes[3].set_xticks([2, 4, 6])
 
     plt.tight_layout()
 
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,6 +3,7 @@ import random
 import operator
 
 import numpy as np
+import matplotlib
 import torch
 
 
@@ -388,3 +389,16 @@ class PrioritizedReplayBuffer(ReplayBuffer):
             self._it_min[idx] = priority ** self._alpha
 
             self._max_priority = max(self._max_priority, priority)
+
+
+class OOMFormatter(matplotlib.ticker.ScalarFormatter):
+    def __init__(self, order=0, fformat="%1.1f", offset=True, mathText=True):
+        self.oom = order
+        self.fformat = fformat
+        matplotlib.ticker.ScalarFormatter.__init__(self,useOffset=offset,useMathText=mathText)
+    def _set_order_of_magnitude(self):
+        self.orderOfMagnitude = self.oom
+    def _set_format(self, vmin=None, vmax=None):
+        self.format = self.fformat
+        if self._useMathText:
+            self.format = r'$\mathdefault{%s}$' % self.format

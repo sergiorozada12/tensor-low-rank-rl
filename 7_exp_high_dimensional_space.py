f12dc9d35f9214ae904967b595dbcf8036ec68d9
sergiorozada12 <sergiorozada12@gmail.com>
2023-01-30 22:22:12 +0100

    feature: final highway experiment added

--- 7_exp_high_dimensional_space.py
+++ 7_exp_high_dimensional_space.py
@@ -4,15 +4,22 @@ from src.experiments.experiments import ExperimentHighway
 env_highway = CustomHighwayEnv()
 
 N_NODES = 20
-PATH_TLR = 'highway_tlr.json'
-PATH_DQN = 'highway_dqn.json'
+PATH_TLR_SMALL = 'highway_tlr_small.json'
+PATH_TLR_LARGE = 'highway_tlr_large.json'
+PATH_DQN_LARGE = 'highway_dqn_large.json'
+PATH_DQN_SMALL = 'highway_dqn_small.json'
 
 
 if __name__ == "__main__":
-    experiment_tlr = ExperimentHighway(PATH_TLR, env_highway, N_NODES)
+    
+    experiment_tlr = ExperimentHighway(PATH_TLR_SMALL, env_highway, N_NODES)
     experiment_tlr.run_experiments(window=100)
 
-    experiment_dqn = ExperimentHighway(PATH_DQN, env_highway, N_NODES)
+    experiment_tlr = ExperimentHighway(PATH_TLR_LARGE, env_highway, N_NODES)
+    experiment_tlr.run_experiments(window=100)
+
+    experiment_dqn = ExperimentHighway(PATH_DQN_SMALL, env_highway, N_NODES)
     experiment_dqn.run_experiments(window=100)
 
-    #nohup python -u 5_exp_methods_comparison.py > nohup.out&
+    experiment_dqn = ExperimentHighway(PATH_DQN_LARGE, env_highway, N_NODES)
+    experiment_dqn.run_experiments(window=100)
